3b8b3e250973af3e14d0af625181a8aa4cc1515a
nandeesh-cdw <131158894+nandeesh-cdw@users.noreply.github.com>
2023-07-13 07:59:15 +0530

    Header,login form , orders page ui created

--- eatz-project/src/app/app.module.ts
+++ eatz-project/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginPageComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule
